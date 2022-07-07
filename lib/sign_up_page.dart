3af865da68383fe43cb736ae5bbac34008b4a6c6
Michael Hathi <michael@numerative.dev>
2022-07-07 12:08:43 +0530

    Complete Sign Up page.

--- lib/sign_up_page.dart
+++ lib/sign_up_page.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:flyy_flutter_plugin/flyy_flutter_plugin.dart';
+import 'package:flyy_invite_earn_flutter/home_page.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 class SignUpPage extends StatefulWidget {
   const SignUpPage({Key? key}) : super(key: key);
@@ -8,8 +11,136 @@ class SignUpPage extends StatefulWidget {
 }
 
 class _SignUpPageState extends State<SignUpPage> {
+  TextEditingController refCodeController = TextEditingController();
+  TextEditingController userIdController = TextEditingController();
+  TextEditingController userNameController = TextEditingController();
+  String referralCode = "", userId = "", userName = "";
+  String refCodeError = "", userIdError = "";
+  final double verticalWidth = 250;
+
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Sign Up"),
+      ),
+      body: SingleChildScrollView(
+        padding: const EdgeInsets.symmetric(horizontal: 24),
+        child: Column(
+          children: [
+            const SizedBox(height: 48),
+            Row(
+              children: [
+                Expanded(
+                  flex: 2,
+                  child: TextField(
+                    controller: refCodeController,
+                    decoration: InputDecoration(
+                        hintText: "Enter Referral Code",
+                        errorText: refCodeError.isEmpty ? null : refCodeError),
+                  ),
+                ),
+                const SizedBox(width: 16),
+                Expanded(
+                  flex: 1,
+                  child: ElevatedButton(
+                    onPressed: () {
+                      bool isValid = validateReferralCode();
+                      setState(() {});
+                      if (isValid) applyReferral(refCodeController.text);
+                    },
+                    child: const Text("Set"),
+                  ),
+                ),
+              ],
+            ),
+            const SizedBox(height: 24),
+            SizedBox(
+              width: verticalWidth,
+              child: TextField(
+                controller: userIdController,
+                decoration: const InputDecoration(hintText: "User Id"),
+              ),
+            ),
+            const SizedBox(height: 16),
+            SizedBox(
+              width: verticalWidth,
+              child: TextField(
+                controller: userNameController,
+                decoration:
+                    const InputDecoration(hintText: "User Name (Optional)"),
+              ),
+            ),
+            const SizedBox(height: 16),
+            SizedBox(
+              width: verticalWidth,
+              child: ElevatedButton(
+                child: const Text("Sign Up"),
+                onPressed: () {
+                  bool isValid = validateUserId();
+                  setState(() {});
+                  if (isValid) {
+                    FlyyFlutterPlugin.setFlyyNewUser(userId);
+                    Navigator.of(context).push(MaterialPageRoute(
+                        builder: (context) => const HomePage()));
+                  }
+                },
+              ),
+            ),
+            const SizedBox(height: 16),
+            TextButton(
+              onPressed: () {
+                SharedPreferences.getInstance().then((value) => value.clear());
+                Navigator.of(context).pop();
+              },
+              child: const Text("Reset SDK Connection"),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+
+  bool validateReferralCode() {
+    bool isValid = false;
+    if (refCodeController.text.isEmpty) {
+      refCodeError = "Referral Code Empty";
+      isValid = false;
+    } else {
+      refCodeError = "";
+      isValid = true;
+    }
+    return isValid;
+  }
+
+  void applyReferral(String referralCode) async {
+    Map<String, dynamic> response =
+        await FlyyFlutterPlugin.verifyReferralCode(referralCode);
+    bool isValid = response["is_valid"];
+
+    if (isValid) {
+      FlyyFlutterPlugin.setFlyyReferralCode(referralCode);
+      if (!mounted) return;
+      const snackBar = SnackBar(content: Text("Referral Code Applied"));
+      ScaffoldMessenger.of(context).showSnackBar(snackBar);
+    } else {
+      if (!mounted) return;
+      const snackBar = SnackBar(content: Text("Invalid Referral Code"));
+      ScaffoldMessenger.of(context).showSnackBar(snackBar);
+    }
+
+    FlyyFlutterPlugin.setFlyyReferralCode(referralCode);
+  }
+
+  bool validateUserId() {
+    bool isValid = false;
+    userId = userIdController.text;
+    if (userId.isEmpty) {
+      isValid = false;
+      userIdError = "User Id Required";
+    } else {
+      isValid = true;
+    }
+    return isValid;
   }
 }
